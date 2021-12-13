6a5c98a840e1912280f30aefa68ece8e0937c377
disys-puspanathan <95684898+disys-puspanathan@users.noreply.github.com>
2021-12-12 21:36:25 -0800

    Add files via upload

---
+++ Swiggy.py
@@ -0,0 +1,166 @@
+restaurants = [{"nonveg":[{"name":"Dindigul Thalapakathi","recommened":"Chicken biryani,price: Rs.289","starters":["Mutton liver, price: Rs.289",
+"Mutton Sukka, price: Rs.319","Kuchi Mutton, price:345","Mutton Egg Roast, price:339","Pallipalayam Chicken Roast, price: Rs329"],"Biryani variety":
+["Thalappakatti Special Mutton Biryani[boneless], price:Rs.359","Mutton Biryani[bone-in],price : Rs.339","Chicken Biryani[Bone-In], price : Rs.289",
+"Chicken 65 Biryani, price: Rs.309"],"rating":5},{"name":"Anjappar","recommened":"Chicken biryani,price: Rs.275","starters":["Mutton Kola,price:Rs.70/perpc",
+"Mutton Sukka Varuval, price: Rs.310","Chicken Lolipop, price:288","Anjappar Poricha Kozhi, price:325","Kadai 65, price: Rs222"],"Biryani variety":
+["Mutton Biryani, price:Rs.325","Chicken Biryani,price : Rs.275","Egg Biryani, price : Rs.225","Chicken 65 Biryani, price: Rs.320","Kaadai Biryani,price:Rs. 360"],
+"Chettinad Special":["Chettinad Kozhi Masala,price:Rs.295","Chettinad Kozhi Varuval,price:Rs.283"],"Breads":["Naan","Parotta"],"rating":5},
+{"name":"Buhari","recommened":"Chicken biryani,price: Rs.289","starters":["Mutton liver, price: Rs.289",
+"Mutton Sukka, price: Rs.319","Kuchi Mutton, price:345","Mutton Egg Roast, price:339","Pallipalayam Chicken Roast, price: Rs329"],"Biryani variety":
+["Thalappakatti Special Mutton Biryani[boneless], price:Rs.359","Mutton Biryani[bone-in],price : Rs.339","Chicken Biryani[Bone-In], price : Rs.289",
+"Chicken 65 Biryani, price: Rs.309"],"rating":3},{"name":"Pandian","recommened":"ChickenFriedrice,price: Rs.289","starters":["Mutton liver, price: Rs.289",
+"Mutton Sukka, price: Rs.319","Kuchi Mutton, price:345","Mutton Egg Roast, price:339","Pallipalayam Chicken Roast, price: Rs329"],"Biryani variety":
+["Thalappakatti Special Mutton Biryani[boneless], price:Rs.359","Mutton Biryani[bone-in],price : Rs.339","Chicken Biryani[Bone-In], price : Rs.289",
+"Chicken 65 Biryani, price: Rs.309"],"rating":4},{"name":"Crescent","recommened":"Chicken biryani,price: Rs.289","starters":["Mutton liver, price: Rs.289",
+"Mutton Sukka, price: Rs.319","Kuchi Mutton, price:345","Mutton Egg Roast, price:339","Pallipalayam Chicken Roast, price: Rs329"],"Biryani variety":
+["Thalappakatti Special Mutton Biryani[boneless], price:Rs.359","Mutton Biryani[bone-in],price : Rs.339","Chicken Biryani[Bone-In], price : Rs.289",
+"Chicken 65 Biryani, price: Rs.309"],"rating":5},{"name":"I Shawarma U","recommened":"Peri Peri Shawarma,price: Rs.289","starters":["Mutton liver, price: Rs.289",
+"Mutton Sukka, price: Rs.319","Kuchi Mutton, price:345","Mutton Egg Roast, price:339","Pallipalayam Chicken Roast, price: Rs329"],"Biryani variety":
+["Thalappakatti Special Mutton Biryani[boneless], price:Rs.359","Mutton Biryani[bone-in],price : Rs.339","Chicken Biryani[Bone-In], price : Rs.289",
+"Chicken 65 Biryani, price: Rs.309"],"rating":5},{"name":"Hotel Al Ali","recommened":"Peri Peri Shawarma,price: Rs.289","starters":["Mutton liver, price: Rs.289",
+"Mutton Sukka, price: Rs.319","Kuchi Mutton, price:345","Mutton Egg Roast, price:339","Pallipalayam Chicken Roast, price: Rs329"],"Biryani variety":
+["Thalappakatti Special Mutton Biryani[boneless], price:Rs.359","Mutton Biryani[bone-in],price : Rs.339","Chicken Biryani[Bone-In], price : Rs.289",
+"Chicken 65 Biryani, price: Rs.309"],"rating":3},{"name":"Hotel Vairam","recommened":"Peri Peri Shawarma,price: Rs.289","starters":["Mutton liver, price: Rs.289",
+"Mutton Sukka, price: Rs.319","Kuchi Mutton, price:345","Mutton Egg Roast, price:339","Pallipalayam Chicken Roast, price: Rs329"],"Biryani variety":
+["Thalappakatti Special Mutton Biryani[boneless], price:Rs.359","Mutton Biryani[bone-in],price : Rs.339","Chicken Biryani[Bone-In], price : Rs.289",
+"Chicken 65 Biryani, price: Rs.309"],"rating":5},{"name":"Lunch Box","recommened":"Peri Peri Shawarma,price: Rs.289","starters":["Mutton liver, price: Rs.289",
+"Mutton Sukka, price: Rs.319","Kuchi Mutton, price:345","Mutton Egg Roast, price:339","Pallipalayam Chicken Roast, price: Rs329"],"Biryani variety":
+["Thalappakatti Special Mutton Biryani[boneless], price:Rs.359","Mutton Biryani[bone-in],price : Rs.339","Chicken Biryani[Bone-In], price : Rs.289",
+"Chicken 65 Biryani, price: Rs.309"],"rating":4},{"name":"Salem Biyani","recommened":"Peri Peri Shawarma,price: Rs.289","starters":["Mutton liver, price: Rs.289",
+"Mutton Sukka, price: Rs.319","Kuchi Mutton, price:345","Mutton Egg Roast, price:339","Pallipalayam Chicken Roast, price: Rs329"],"Biryani variety":
+["Thalappakatti Special Mutton Biryani[boneless], price:Rs.359","Mutton Biryani[bone-in],price : Rs.339","Chicken Biryani[Bone-In], price : Rs.289",
+"Chicken 65 Biryani, price: Rs.309"],"rating":3},{"name":"Bowl Company","recommened":"Peri Peri Shawarma,price: Rs.289","starters":["Mutton liver, price: Rs.289",
+"Mutton Sukka, price: Rs.319","Kuchi Mutton, price:345","Mutton Egg Roast, price:339","Pallipalayam Chicken Roast, price: Rs329"],"Biryani variety":
+["Thalappakatti Special Mutton Biryani[boneless], price:Rs.359","Mutton Biryani[bone-in],price : Rs.339","Chicken Biryani[Bone-In], price : Rs.289",
+"Chicken 65 Biryani, price: Rs.309"],"rating":5}]}]
+
+
+for i in restaurants:
+    if i["nonveg"][0]["rating"]>=4:
+            if i["nonveg"][0]["name"] == "Dindigul Thalapakathi":
+                print("\nDindigul Thalapakathi:")
+                print("Starters:")
+                print(restaurants[0]["nonveg"][0]["starters"])
+                print("\nBiryani Variety:")
+                print(restaurants[0]["nonveg"][0]["Biryani variety"])
+                print("\nRecommened Food:")
+                print(restaurants[0]["nonveg"][0]["recommened"])
+
+                
+for i in restaurants:
+    if i["nonveg"][1]["rating"]>=4:
+            if i["nonveg"][1]["name"] == "Anjappar":
+                print("\nAnjappar:")
+                print("Starters:")
+                print(restaurants[0]["nonveg"][1]["starters"])
+                print("\nBiryani Variety:")
+                print(restaurants[0]["nonveg"][1]["Biryani variety"])
+                print("\nChettinad Special:")
+                print(restaurants[0]["nonveg"][1]["Chettinad Special"])
+                print("\nRecommened Food:")
+                print(restaurants[0]["nonveg"][1]["recommened"])
+
+                
+for i in restaurants:
+    if i["nonveg"][2]["rating"]>=4:
+            if i["nonveg"][2]["name"] == "Buhari":
+                print("\nBuhari:")
+                print("Starters:")
+                print(restaurants[0]["nonveg"][2]["starters"])
+                print("\nBiryani Variety:")
+                print(restaurants[0]["nonveg"][2]["Biryani variety"])
+                print("\nRecommened Food:")
+                print(restaurants[0]["nonveg"][2]["recommened"])
+
+                
+for i in restaurants:
+    if i["nonveg"][3]["rating"]>=4:
+            if i["nonveg"][3]["name"] == "Pandian":
+                print("\nPandian:")
+                print("Starters:")
+                print(restaurants[0]["nonveg"][3]["starters"])
+                print("\nBiryani Variety:")
+                print(restaurants[0]["nonveg"][3]["Biryani variety"])
+                print("\nRecommened Food:")
+                print(restaurants[0]["nonveg"][3]["recommened"])
+
+for i in restaurants:
+    if i["nonveg"][4]["rating"]>=4:
+            if i["nonveg"][4]["name"] == "Crescent":
+                print("\nCrescent:")
+                print("Starters:")
+                print(restaurants[0]["nonveg"][4]["starters"])
+                print("\nBiryani Variety:")
+                print(restaurants[0]["nonveg"][4]["Biryani variety"])
+                print("\nRecommened Food:")
+                print(restaurants[0]["nonveg"][4]["recommened"])
+
+
+for i in restaurants:
+    if i["nonveg"][5]["rating"]>=4:
+            if i["nonveg"][5]["name"] == "I Shawarma U":
+                print("\nI Shawarma U:")
+                print("Starters:")
+                print(restaurants[0]["nonveg"][5]["starters"])
+                print("\nBiryani Variety:")
+                print(restaurants[0]["nonveg"][5]["Biryani variety"])
+                print("\nRecommened Food:")
+                print(restaurants[0]["nonveg"][5]["recommened"])
+
+
+for i in restaurants:
+    if i["nonveg"][6]["rating"]>=4:
+            if i["nonveg"][6]["name"] == "Hotel Al Ali":
+                print("\nHotel Al Ali:")
+                print("Starters:")
+                print(restaurants[0]["nonveg"][6]["starters"])
+                print("\nBiryani Variety:")
+                print(restaurants[0]["nonveg"][6]["Biryani variety"])
+                print("\nRecommened Food:")
+                print(restaurants[0]["nonveg"][6]["recommened"])
+
+for i in restaurants:
+    if i["nonveg"][7]["rating"]>=4:
+            if i["nonveg"][7]["name"] == "Hotel Vairam":
+                print("\nHotel Vairam:")
+                print("Starters:")
+                print(restaurants[0]["nonveg"][7]["starters"])
+                print("\nBiryani Variety:")
+                print(restaurants[0]["nonveg"][7]["Biryani variety"])
+                print("\nRecommened Food:")
+                print(restaurants[0]["nonveg"][7]["recommened"])
+
+for i in restaurants:
+    if i["nonveg"][8]["rating"]>=4:
+            if i["nonveg"][8]["name"] == "Lunch Box":
+                print("\nLunch Box:")
+                print("Starters:")
+                print(restaurants[0]["nonveg"][8]["starters"])
+                print("\nBiryani Variety:")
+                print(restaurants[0]["nonveg"][8]["Biryani variety"])
+                print("\nRecommened Food:")
+                print(restaurants[0]["nonveg"][8]["recommened"])                
+
+
+for i in restaurants:
+    if i["nonveg"][9]["rating"]>=4:
+            if i["nonveg"][9]["name"] == "Salem Biyani":
+                print("\nSalem Biyani:")
+                print("Starters:")
+                print(restaurants[0]["nonveg"][9]["starters"])
+                print("\nBiryani Variety:")
+                print(restaurants[0]["nonveg"][9]["Biryani variety"])
+                print("\nRecommened Food:")
+                print(restaurants[0]["nonveg"][9]["recommened"])
+
+
+for i in restaurants:
+    if i["nonveg"][10]["rating"]>=4:
+            if i["nonveg"][10]["name"] == "Bowl Company":
+                print("\nBowl Company:")
+                print("Starters:")
+                print(restaurants[0]["nonveg"][10]["starters"])
+                print("\nBiryani Variety:")
+                print(restaurants[0]["nonveg"][10]["Biryani variety"])
+                print("\nRecommened Food:")
+                print(restaurants[0]["nonveg"][10]["recommened"])
